d3a7c0885ca4863074e49e5197c529cad56e7f71
Вылегжанин Андрей <andre-vylegzhani@yandex.ru>
2021-01-28 09:16:30 +0300

    Добавьте файлы проекта.

---
+++ Tehno/Technics/ComputerTechnics.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tehno.Technics
+{
+    public abstract class ComputerTechnics
+    {
+        public abstract string GetName { get; protected set; }
+        public abstract string GetCostCost { get; protected set; }
+    }
+}
